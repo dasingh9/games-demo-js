b83740e405ee70f003c1415df757b54ad81cc57a
Davinder Singh <dasingh9@gmail.com>
2025-07-17 21:09:01 +1200

    Use Template and improve look and feel of Games site

--- components/games-container.js
+++ components/games-container.js
@@ -1,19 +1,21 @@
 class GamesContainer {
-    getGameCards(games) {
-        let gamesHtml = "";
+    // Returns array of DOM elements using <template>
+    getGameCardElements(games) {
+        const template = document.getElementById('game-card-template');
+        const cards = [];
         games.forEach(game => {
-            const gameHtml =
-            `<div class="card">
-                <img src="${game.thumbnail}" alt="">
-                <div class="card-body">
-                    <h3>${game.title}</h3>
-                    <span>${game.genre}</span>
-                    <a href="${game.gameUrl}">Play</a>
-                </div>
-            </div>`;
-
-            gamesHtml += gameHtml;
+            const clone = template.content.cloneNode(true);
+            const card = clone.querySelector('.card');
+            const img = card.querySelector('img');
+            img.src = game.thumbnail;
+            img.alt = game.title;
+            card.querySelector('.game-title').textContent = game.title;
+            card.querySelector('.game-description').textContent = game.genre;
+            const playBtn = card.querySelector('.play-btn');
+            playBtn.href = game.gameUrl;
+            playBtn.textContent = 'Play';
+            cards.push(card);
         });
-        return gamesHtml;
+        return cards;
     }
 }
